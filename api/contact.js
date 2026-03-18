97b81cf4b2e2cd8d9dae6c24318a6819ecc27408
emilia grama <emiliagrama@gmail.com>
2026-03-18 11:27:06 +0100

    debug contact api

--- api/contact.js
+++ api/contact.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
 
     const result = await resend.emails.send({
       from: "Hugo Site <onboarding@resend.dev>",
-      to: "emiliagrama@gmail.com",
+      to: "figuerahugo.gmail.com",
       replyTo: email.trim(),
       subject: `New message from ${name.trim()}`,
       html: `
